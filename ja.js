a57c7687b2358d2bbb625b2c885aa3fc2fe14b26
InputMiazz <chrissybanuelos@yahoo.com>
2021-05-18 21:15:58 -0700

    Improving randomness

--- ja.js
+++ ja.js
@@ -1,17 +1,33 @@
 
+function coordsGen(){
+  coordinates()
+  coordinatestwo()
 
-function coordinates(){
-
-
-  firstDigits = Math.floor(Math.random() * 100)
-  lastDigits = Math.floor(Math.random() * 19999)
-
-
-  afirstDigits = Math.floor(Math.random() * 100)
-  blastDigits = Math.floor(Math.random() * 19999)
-
-  document.getElementById('b-coordinates').innerHTML = `${firstDigits}.${lastDigits}, ${afirstDigits}.${blastDigits}`;
+  document.getElementById('b-coordinates').innerHTML = `${
+twoDigitsPartsA} ${twoDigitsPartsB} `;
 
   document.getElementById('c-coordinates').style.display = 'none';
   document.getElementById('b-coordinates').style.display = 'block';
+}
+
+
+
+function coordinates(){
+
+  firstCords =  Math.ceil(Math.random() * (999999 - 62222 + 1) + 20)
+
+  twoDigitsPartsA = firstCords.toString().split(".");
+  twoDigitsPartsA[0] = twoDigitsPartsA[0].replace(/\B(?=(\d{2})+(?!(\d)))/, ".");
+  twoDigitsPartsA.join(".");
+}
+
+
+
+function coordinatestwo(){
+
+    secondCords = Math.ceil(Math.random() * (999999 - 62222 + 1) + 20)
+
+  twoDigitsPartsB = secondCords.toString().split(".");
+  twoDigitsPartsB[0] = twoDigitsPartsB[0].replace(/\B(?=(\d{2})+(?!(\d)))/, ".");
+  twoDigitsPartsB.join(".");
 }
